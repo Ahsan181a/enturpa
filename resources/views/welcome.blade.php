806adf181c6328ad2166b561907e5ee270d9af27
Ahsan181a <ahsanbondhan181@gmail.com>
2021-12-30 08:55:57 +0600

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,93 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <link rel="stylesheet" href="css/style.css">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+    <title>Hello, world!</title>
+  </head>
+  <body>
+   <div id="bgImage">
+
+       <nav class="navbar navbar-expand-lg navbar-light bg-success">
+  <a class="navbar-brand" href="#">E-Entrepreneurship
+</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mx-auto">
+      <li class="nav-item ">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">About</a>
+      </li>
+        <li class="nav-item">
+        <a class="nav-link" href="#">Contact Us</a>
+      </li>
+         <li class="nav-item px-4 dropdown">
+    <a class="nav-link dropdown-toggle" href="#" id="servicesDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Services</a>
+    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="servicesDropdown">
+     <a class="dropdown-item" href="#">What we do</a>
+     <a class="dropdown-item" href="#">How we fit your needs</a>
+     <div class="dropdown-divider"></div>
+     <div class="d-md-flex align-items-start justify-content-start">
+      <div>   
+       <div class="dropdown-header">Development</div>
+       <a class="dropdown-item" href="#">Bespoke software</a>
+       <a class="dropdown-item" href="#">Mobile apps</a>
+       <a class="dropdown-item" href="#">Websites</a>
+      </div>
+      <div>
+       <div class="dropdown-header">Professional Services</div>
+       <a class="dropdown-item" href="#">Project rescue</a>
+       <a class="dropdown-item" href="#">Source code recovery</a>
+       <a class="dropdown-item" href="#">Application support &amp; maintenance</a>
+      </div>
+      <div>
+       <div class="dropdown-header">Fixed Price Services</div>
+       <a class="dropdown-item" href="#">Add cookie consent</a>
+       <a class="dropdown-item" href="#">Add captcha</a>
+       <a class="dropdown-item" href="#">Add core data</a>
+       <a class="dropdown-item" href="#">Custom error pages</a>
+       <a class="dropdown-item" href="#">Contact form creation</a>
+       <a class="dropdown-item" href="#">Automated backups</a>
+       <a class="dropdown-item" href="#">Image to HTML</a>
+      </div>
+     </div>
+    </div>
+   </li>
+   <li class="nav-item px-4">
+    <a href="#" class="nav-link">Help Center</a>
+   </li>
+   <li class="nav-item px-4">
+    <a href="#" class="nav-link">Content</a>
+   </li>
+   <li class="nav-item px-4">
+    <a href="#" class="nav-link">FAQ</a>
+   </li>
+   
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+     
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
+  </div>
+</nav>
+
+   </div>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+  </body>
+</html>
